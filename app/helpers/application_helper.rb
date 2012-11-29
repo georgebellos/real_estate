bacd4fc0644696712e3aa8ca33b48e14e5be64a2
George Bellos <georgebellos@me.com>
2012-11-29 21:44:08 +0200

    add title, description meta info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def description(page_description)
+    content_for(:description) { page_description }
+  end
+
+  def yield_or_default(section, default='')
+    content_for?(section) ? content_for(section) + " | " + default : default
+  end
 end
