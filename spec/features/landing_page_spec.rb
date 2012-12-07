839d880be69bd75f02f47430f2a17c22a7a29865
George Bellos <georgebellos@me.com>
2012-12-07 21:23:59 +0200

    correct spec/features directory name

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -4,14 +4,18 @@ feature 'Home Page', %q{
   As an unregistered user
   I want to find information about the apllication
 } do
+  background { visit root_path }
   scenario "Application About information" do
-    visit root_path
     click_link 'About'
     expect(page).to have_content('About Real Estate')
   end
+
   scenario "Application Contact Information" do
-    visit root_path
     click_link 'Contact'
     expect(page).to have_content('Contact')
+  end
+
+  scenario "Application Sign Up information" do
+    expect(page).to have_content('Sign Up')
   end
 end
