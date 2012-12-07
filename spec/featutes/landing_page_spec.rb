27102df52e7be1c8757a3d56f6b5693eba8173a0
George Bellos <georgebellos@me.com>
2012-12-07 18:34:18 +0200

    change home page spec description

--- spec/featutes/landing_page_spec.rb
+++ spec/featutes/landing_page_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
-feature 'A User visits the Home Page' do
-  scenario "Finds 'About and Contact' information for the application" do
+feature 'Home Page', %q{
+  As an unregistered user
+  I want to find information about the apllication
+} do
+  scenario "Application About information" do
     visit root_path
     click_link 'About'
-    expect(page).to have_content('About')
+    expect(page).to have_content('About Real Estate')
+  end
+  scenario "Application Contact Information" do
     visit root_path
     click_link 'Contact'
     expect(page).to have_content('Contact')
