79a203482b0703ca1463d20a3cbab5bc295cdb9a
George Bellos <georgebellos@me.com>
2012-12-09 15:22:37 +0200

    add basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20121208143430) do
+
+  create_table "users", :force => true do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
