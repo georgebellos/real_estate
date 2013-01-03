a0378a4790f2f81720a515915f8bf24e9b64cffe
George Bellos <georgebellos@me.com>
2013-01-03 15:49:23 +0200

    use different directory for storing images during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -76,6 +76,28 @@ Spork.prefork do
       DatabaseCleaner.clean
     end
 
+    #use different directories for file uploads
+    if defined?(CarrierWave)
+      CarrierWave::Uploader::Base.descendants.each do |klass|
+        next if klass.anonymous?
+        klass.class_eval do
+          def cache_dir
+            "#{Rails.root}/spec/support/uploads/tmp"
+          end
+
+          def store_dir
+            "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+          end
+        end
+      end
+    end
+
+    # delete image uploads
+    config.after(:all) do
+      if Rails.env.test?
+        FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+      end
+    end
   end
 end
 
