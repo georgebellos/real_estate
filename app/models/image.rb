33f8935a94611935c64a28618d7e154c91ddcdce
George Bellos <georgebellos@me.com>
2013-01-03 16:16:08 +0200

    store only images with type png jpeg jpg gif

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,8 @@
 class Image < ActiveRecord::Base
   belongs_to :property
-
   attr_accessible :attachment
 
   mount_uploader :attachment, PictureUploader
+
+  validates :attachment, presence: true
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -4,6 +4,22 @@ describe Image do
   include CarrierWave::Test::Matchers
 
   it 'has a valid factory' do
-    expect(build :image).to be_valid
+    expect(create :image).to be_valid
+  end
+
+  it 'is invalid without an attachment' do
+    expect(build :image, attachment: nil).not_to be_valid
+  end
+
+  %w{ jpeg jpg gif png }.each do |type|
+    it "is valid with type: #{type}" do
+      expect(build :image, attachment: File.open(
+    Rails.root + "spec/support/files/property.#{type}")).to be_valid
+    end
+  end
+
+  it "is invalid with type other than 'png, jpeg, jpg, gif'" do
+    expect(build :image, attachment: File.open(
+      Rails.root + 'spec/support/files/wrong_file_type.txt')).not_to be_valid
   end
 end
