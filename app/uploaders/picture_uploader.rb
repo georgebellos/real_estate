fbc0293baf3d5bc07bafa27fab80a1db440f700f
George Bellos <georgebellos@me.com>
2013-01-24 15:57:15 +0200

    add image versions for a property listing

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -29,16 +29,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+   process resize_to_fill: [500, 500]
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+   version :small do
+     process resize_to_fill: [200, 200]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -27,4 +27,26 @@ describe Image do
     expect(build :image, attachment: File.open(
       Rails.root + 'spec/support/files/over_2mb_image.png')).not_to be_valid
   end
+
+  context 'versions' do
+    before do
+      PictureUploader.enable_processing = true
+    end
+
+    after do
+      PictureUploader.enable_processing = false
+    end
+
+    describe 'small' do
+      it 'scales down an image to be exactly 200 by 200 pixels' do
+        expect((create :image).attachment.small).to have_dimensions(200,200)
+      end
+    end
+
+    describe 'normal' do
+      it 'scales down an image bigger than 500 by 500 pixel to be exactly 500 by 500 pixels' do
+        expect((create :image).attachment).to have_dimensions(500,500)
+      end
+    end
+  end
 end
