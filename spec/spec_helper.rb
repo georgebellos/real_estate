eee03354376b6dbc0f4a191367b51c556c573034
George Bellos <georgebellos@me.com>
2013-01-28 20:22:44 +0200

    fix clean up attachments after rspec tests
    zeus or spring still doesn't pick new directories for
    attachments.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,26 +78,26 @@ prefork = lambda do
       DatabaseCleaner.clean
     end
 
-    #use different directories for file uploads
-    if defined?(CarrierWave)
-      CarrierWave::Uploader::Base.descendants.each do |klass|
-        next if klass.anonymous?
-        klass.class_eval do
-          def cache_dir
-            "#{Rails.root}/spec/support/uploads/tmp"
-          end
-
-          def store_dir
-            "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-          end
-        end
-      end
-    end
-
     # delete image uploads
     config.after(:all) do
       if Rails.env.test?
         FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+      end
+    end
+  end
+
+  #use different directories for file uploads
+  if defined?(CarrierWave)
+    CarrierWave::Uploader::Base.descendants.each do |klass|
+      next if klass.anonymous?
+      klass.class_eval do
+        def cache_dir
+          "#{Rails.root}/spec/support/uploads/tmp"
+       end
+
+        def store_dir
+          "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+        end
       end
     end
   end
