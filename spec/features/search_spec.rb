114611aa77344696f8ddae91675519e8e03412bd
George Bellos <georgebellos@me.com>
2013-02-08 00:48:22 +0200

    filter search results by type, price, floor size and bedrooms

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -26,4 +26,35 @@ feature 'Search', :elasticsearch do
     click_button 'Quick Search'
     expect(page).to have_content('No property listings were found with this search criteria')
   end
+
+  scenario 'Filter properties by type' do
+    find(:xpath, "//fieldset/input[@value='Triplex']").set(true)
+    click_button 'Filter'
+    expect(page).to have_content('Doiranis')
+    expect(page).not_to have_content('Kefalinias')
+  end
+
+  scenario 'Filter properties by price' do
+    fill_in 'search[price_from]', with: 0
+    fill_in 'search[price_to]', with: 600
+    click_button 'Filter'
+    expect(page).to have_content('Doiranis')
+    expect(page).not_to have_content('Kefalinias')
+  end
+
+  scenario 'Filter properties by bedrooms' do
+    fill_in 'search[bedrooms_from]', with: 0
+    fill_in 'search[bedrooms_to]', with: 5
+    click_button 'Filter'
+    expect(page).to have_content('Doiranis')
+    expect(page).not_to have_content('Kefalinias')
+  end
+
+  scenario 'Filter properties by floor size' do
+    fill_in 'search[floor_size_from]', with: 0
+    fill_in 'search[floor_size_to]', with: 400
+    click_button 'Filter'
+    expect(page).to have_content('Doiranis')
+    expect(page).not_to have_content('Kefalinias')
+  end
 end
