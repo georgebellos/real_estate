3ee92e91728afa3eb4bb6f86663e4ef68b83d7eb
George Bellos <georgebellos@me.com>
2013-02-22 11:09:31 +0200

    add #title method for properties

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -5,7 +5,7 @@ describe Property do
     expect(create :property).to be_valid
   end
 
-  %w{floor_size price status bedroom bathroom parking street street_number city year category }.each do |attr|
+  %w{ floor_size price status bedroom bathroom parking street street_number city year category }.each do |attr|
     it "is invalid without #{ attr }" do
       expect(build :property, attr.to_sym => nil).not_to be_valid
     end
@@ -16,11 +16,17 @@ describe Property do
     end
   end
 
-  it 'is invalid with a summary longer than 400 characters' do
-    expect(build :property, summary: "a" * 401).not_to be_valid
+  it 'is invalid with a summary longer than 800 characters' do
+    expect(build :property, summary: "a" * 801).not_to be_valid
   end
 
   it 'is invalid with a construction year before 1900' do
     expect(build :property, year: 1850).not_to be_valid
+  end
+
+  describe "#title" do
+    it 'retuns the property status and category as a full string' do
+      expect((build :property).title).to eql "Rent Apartment"
+    end
   end
 end
