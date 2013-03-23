400a46624de215319f676f20d8de2539ab1e3c91
George Bellos <georgebellos@me.com>
2013-03-23 20:54:50 +0200

    integration test for log in via twitter, facebook, google

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -50,3 +50,29 @@ feature 'User sign up via services' do
     expect(page).to have_content('Could not authenticate you from Google')
   end
 end
+
+feature 'User log in via services' do
+  scenario 'Log in via Twitter' do
+    set_omniauth
+    visit root_path
+    click_link 'Login'
+    click_link 'Log in with Twitter'
+    expect(page).to have_content('Signed in!')
+  end
+
+  scenario 'Log in via Facebook' do
+    set_omniauth(:facebook)
+    visit root_path
+    click_link 'Login'
+    click_link 'Log in with Facebook'
+    expect(page).to have_content('Signed in!')
+  end
+
+  scenario 'Log in via Google' do
+    set_omniauth(:google_oauth2)
+    visit root_path
+    click_link 'Login'
+    click_link 'Log in with Google'
+    expect(page).to have_content('Signed in!')
+  end
+end
