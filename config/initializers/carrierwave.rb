1ea6ec88164af6afa97ff627f12b7554111a702b
George Bellos <georgebellos@me.com>
2013-03-25 20:16:15 +0200

    use amazon s3 via fog in production for storing images
    change fog storage
    carrierwave settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,8 @@
-if Rails.env.test?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.test? || Rails.env.development?
     config.storage = :file
     config.enable_processing = false
+  else
+    config.storage = :fog
   end
 end
