295aba92126547a71277c1fae154811a7f8092c8
George Bellos <georgebellos@me.com>
2013-03-26 19:16:36 +0200

    use different region bitbucket for storage

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,14 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJRBSK2I6W3XBE7KQ',                        # required
-    :aws_secret_access_key  => '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'                        # required
-    #:region                 => 'eu-west-1'                  # optional, defaults to 'us-east-1'
-    #:host                   => 's3.example.com',             # optional, defaults to nil
-    #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    provider:                'AWS',
+    aws_access_key_id:       ENV['S3_ACCESS_KEY'],
+    aws_secret_access_key:   ENV['S3_SECRET_ACCESS_KEY'],
+    region:                  'eu-west-1'
   }
 
-  config.fog_directory  = 'katikia'                     # required
-  config.fog_public     = true                                   # optional, defaults to true
-  #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.fog_directory  = 'ekatikia'
 end
