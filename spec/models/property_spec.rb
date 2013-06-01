58ef4d61a805316c00e62037cffaf8d1e6332d21
George Bellos <georgebellos@me.com>
2013-06-01 10:58:48 +0300

    Use detailed expectations for property tests

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -5,11 +5,13 @@ describe Property do
     expect(create :property).to be_valid
   end
 
-  %w{ floor_size price status bedroom bathroom parking street street_number city year category }.each do |attr|
+  %w{ floor_size price status bedroom bathroom parking
+      street street_number city year category }.each do |attr|
     it "is invalid without #{ attr }" do
       expect(build :property, attr.to_sym => nil).not_to be_valid
     end
   end
+
   %w{ floor_size bathroom bedroom parking street_number }.each do |attr|
     it "is invalid when #{ attr } is not a number " do
       expect(build :property, attr.to_sym => "string").not_to be_valid
@@ -17,15 +19,15 @@ describe Property do
   end
 
   it 'is invalid with a summary longer than 800 characters' do
-    expect(build :property, summary: "a" * 801).not_to be_valid
+    expect(build :property, summary: "a" * 801).to have(1).errors_on(:summary)
   end
 
   it 'is invalid with a construction year before 1900' do
-    expect(build :property, year: 1850).not_to be_valid
+    expect(build :property, year: 1850).to have(1).errors_on(:year)
   end
 
   describe "#title" do
-    it 'retuns the property status and category as a full string' do
+    it 'returns the property status and category as a full string' do
       expect((build :property).title).to eql "Rent Apartment"
     end
   end
