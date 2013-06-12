9486ab1d36d5d448867a3624002a8a4b7fcb2f39
George Bellos <georgebellos@me.com>
2013-06-12 20:00:41 +0300

    Add more content to footer

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -1,25 +1,5 @@
 require 'spec_helper'
 
-feature 'Home Page', %q{
-  As an unregistered user
-  I want to find information about the apllication
-} do
-  background { visit root_path }
-  scenario "Application About information" do
-    click_link 'About'
-    expect(page).to have_content('About Real Estate')
-  end
-
-  scenario "Application Contact Information" do
-    click_link 'Contact'
-    expect(page).to have_content('Contact')
-  end
-
-  scenario "Application Sign Up information" do
-    expect(page).to have_content('Sign Up')
-  end
-end
-
 feature 'Latest entries', %q{
   As an unregistered user
   When i go to the landing page
