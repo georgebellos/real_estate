bb3596e21333d1aa4aa6e900e855d91f56884dd7
George Bellos <georgebellos@me.com>
2013-06-18 17:18:12 +0300

    Remove secret session token from repository

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Katikia::Application.config.secret_token = '04abc46a4be91e4c201b0b875357083ca08c34d19812c32e81fd8faaf1d14fba7326638a7b21a125f9bc75a2651c82f8f4b7d81c0d45844fcfd79d22af1406d7'
+Katikia::Application.config.secret_token = if Rails.env.development? || Rails.env.test?
+                                             'g' * 30
+                                           else
+                                             ENV['SESSION_SECRET_TOKEN']
+                                           end
