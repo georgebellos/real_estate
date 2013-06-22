b805cb8b34383e746f8a3b08db2267bc6627427f
George Bellos <georgebellos@me.com>
2013-06-22 19:53:52 +0300

    Add factory image with versions
    Create versions of an image only for this factory

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,11 +1,16 @@
 FactoryGirl.define do
   factory :image do
-    process_attachment_upload  true
-    attachment { File.open(Rails.root + 'spec/support/files/property.png') }
     property
+    PictureUploader.enable_processing = false
+    process_attachment_upload true
+    attachment { File.open(Rails.root + 'spec/support/files/property.png') }
 
     factory :invalid_image do
       attachment { File.open(Rails.root + 'spec/support/files/wrong_file_type.txt')}
+    end
+
+    factory :image_with_versions do
+      PictureUploader.enable_processing = true
     end
   end
 end
